58067ea82c72ca81589e8f695991a2f7e0c1100f
Nevo Aftabi <curryboy1245@gmail.com>
2025-11-26 17:59:25 -0500

    MongoDB timeout fix

--- db/mongo.js
+++ db/mongo.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
 
 module.exports = async function connectMongo() {
-  await mongoose.connect(process.env.MONGO_URL);
+  await mongoose.connect(
+    process.env.MONGO_URL, { 
+    serverSelectionTimeoutMS: 5000,   
+    socketTimeoutMS: 45000 
+  }
+  );
   console.log('MongoDB started');
 };
